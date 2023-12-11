aa8b658d9feb7b017315c39bef72b49f09f75c91
hchiba1984 <h.chiba201103@gmail.com>
2023-12-11 15:24:14 +0900

    SQLを埋め込みに修繕した

--- src/Web/Api.Kashilog/Repositories/Enterprise/Companies/CompanyRepository.cs
+++ src/Web/Api.Kashilog/Repositories/Enterprise/Companies/CompanyRepository.cs
@@ -1,20 +1,57 @@
-using Api.Kashilog.Repositories.Enterprise.Companies.Sqls;
 using Database.Kashilog.DbContexts;
 using DomainObject.Kashilog.Enterprise;
 using ORMIntegrator;
 using Service.Extensions.DependencyInjection.Markers;
 
-namespace Api.Kashilog.Repositories.Enterprise.Companies {
-    public class CompanyRepository : IRepository {
-        private SqlManager<KashilogContext> SqlManager { get; }
+namespace Api.Kashilog.Repositories.Enterprise.Companies;
 
-        public CompanyRepository(SqlManager<KashilogContext> sqlManager) =>
-            SqlManager = sqlManager;
+public class CompanyRepository(SqlManager<KashilogContext> sqlManager) : IRepository {
+    SqlManager<KashilogContext> SqlManager { get; } = sqlManager;
 
-        public Task<IEnumerable<Company>> FindAllCompanyAsync() => SqlManager.SelectAsync<Company>(SqlForCompanyResource.FindAllCompany);
+    public Task<IEnumerable<Company>> FindAllCompanyAsync() =>
+        SqlManager.SelectAsync<Company>($"""
+            SELECT
+                CompanyId			AS CompanyId,
+                CompanyName			AS CompanyName,
+                ZipCode				AS ZipCode,
+                Country				AS Country,
+                City				AS City,
+                Address				AS Address,
+                TelephoneNumber		AS TelephoneNumber
+            FROM
+                enterprise.CmnCompany
+            """);
 
-        public Task<IEnumerable<Company>> FindCompanyByIdAsync(int id) => SqlManager.SelectAsync<Company>(SqlForCompanyResource.FindCompanyById, new { Id = id });
+    public Task<IEnumerable<Company>> FindCompanyByIdAsync(int id) =>
+        SqlManager.SelectAsync<Company>($"""
+            SELECT
+                CompanyId			AS CompanyId,
+                CompanyName			AS CompanyName,
+                ZipCode				AS ZipCode,
+                Country				AS Country,
+                City				AS City,
+                Address				AS Address,
+                TelephoneNumber		AS TelephoneNumber
+            FROM
+                enterprise.CmnCompany
+            WHERE
+                CompanyId = @Id
+            """,
+            new { Id = id });
 
-        public Task<IEnumerable<Company>> FindCompanyInIdsAsync(IEnumerable<int> ids) => SqlManager.SelectAsync<Company>(SqlForCompanyResource.FindCompanyInIds, new { Ids = ids });
-    }
+    public Task<IEnumerable<Company>> FindCompaniesInIdsAsync(IEnumerable<int> ids) =>
+        SqlManager.SelectAsync<Company>($"""
+            SELECT
+                CompanyId			AS CompanyId,
+                CompanyName			AS CompanyName,
+                ZipCode				AS ZipCode,
+                Country				AS Country,
+                City				AS City,
+                Address				AS Address,
+                TelephoneNumber		AS TelephoneNumber
+            FROM
+                enterprise.CmnCompany
+            WHERE
+                CompanyId IN @Ids
+            """, new { Ids = ids });
 }
