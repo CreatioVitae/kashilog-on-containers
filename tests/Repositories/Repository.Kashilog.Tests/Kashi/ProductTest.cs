24b9e2247c735a947974ef575fa3e19c627824cb
hchiba1984 <h.chiba201103@gmail.com>
2023-12-12 15:09:41 +0900

    nailcare.

--- tests/Repositories/Repository.Kashilog.Tests/Kashi/ProductTest.cs
+++ tests/Repositories/Repository.Kashilog.Tests/Kashi/ProductTest.cs
@@ -36,6 +36,7 @@ public class ProductTest(RepositoryKashilogTestContext testContext) : IDisposabl
 
         Assert.Equal(expected.ProductId, product.ProductId);
         Assert.Equal(expected.ProductRevision, product.ProductRevision);
+        Assert.Equal(expected.ValidBeginDateTime, product.ValidBeginDateTime);
 
         //Assert.Equal(
         //    new () {
