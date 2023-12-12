3e3fe32fe1ff3689b63a3765ecfcce491df660c4
hchiba1984 <h.chiba201103@gmail.com>
2023-12-12 20:22:32 +0900

    Add repository test (#19)

---
+++ tests/Repositories/Repository.Kashilog.Tests/Kashi/ProductTest.cs
@@ -0,0 +1,38 @@
+using DomainObject.Kashilog.ConstantValues.Kashi;
+using Repository.Kashilog.Kashi;
+using Repository.Kashilog.Tests.TestContexts;
+
+namespace Repository.Kashilog.Tests.Kashi;
+public class ProductTest(RepositoryKashilogTestContext testContext) : IDisposable, IClassFixture<RepositoryKashilogTestContext> {
+    RepositoryKashilogTestContext TestContext { get; } = testContext;
+
+    [Fact(DisplayName = "Idを指定して製品を取得する。正常完了ケース")]
+    public async Task FindProductByIdAsyncTest_CaseComplete() {
+        await using var sqlManager = TestContext.GetSqlManager();
+
+        var repository = new ProductsRepository(sqlManager);
+        var actual = await repository.FindProductByIdAsync(1);
+        
+        Assert.Equal(
+            new() {
+                ProductId = 1,
+                ProductRevision = 1,
+                ValidBeginDateTime = new DateTime(1966, 1, 1),
+                ValidEndDateTime = new DateTime(2999, 12, 31),
+                ProductName = "ポッキーチョコレート",
+                LargeCategory = LargeCategory.TheWestConfectionery,
+                MiddleCategory = MiddleCategory.Chocolate,
+                SmallCategory = SmallCategory.QuasiChocolate,
+                UnitPrice = 278.0000M,
+                Amount = 9.00M,
+                AmountType = AmountType.Pack,
+                Description = $"軽快な食感に焼き上げたプレッツェルにコクのあるチョコレートをコーティング。{Environment.NewLine}ポキッとした心地よい食感のポッキーは、楽しさいっぱいの弾むおいしさです。",
+                MakerCompanyId = 1,
+                PublisherCompanyId = 1
+            },
+            actual);
+    }
+
+    public void Dispose() =>
+        GC.SuppressFinalize(this);
+}
