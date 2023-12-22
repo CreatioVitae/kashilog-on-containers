cef8fd59bc46cb755c5e993dd2eca4ac8e950bb1
hchiba1984 <h.chiba201103@gmail.com>
2023-12-22 11:19:26 +0900

    custom validatorをinstallした

--- src/Domain/DomainObjects/Kashilog/Kashi/CommandParameters/ProductCreateParameters.cs
+++ src/Domain/DomainObjects/Kashilog/Kashi/CommandParameters/ProductCreateParameters.cs
@@ -1,13 +1,16 @@
 using DomainObject.Kashilog.ConstantValues.Kashi;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace DomainObject.Kashilog.Kashi.CommandParameters;
 public record ProductCreateParameters {
     [Required]
+    [CustomValidation(typeof(ProductCreateParameters), nameof(ValidateValidBeginDateTime))]
     public DateTime ValidBeginDateTime { get; init; }
 
     [Required]
+    [CustomValidation(typeof(ProductCreateParameters), nameof(ValidateValidEndDateTime))]
     public DateTime ValidEndDateTime { get; init; }
 
     [Required]
@@ -40,4 +43,15 @@ public record ProductCreateParameters {
 
     [Required]
     public int PublisherCompanyId { get; init; }
+
+    public static ValidationResult? ValidateValidBeginDateTime(DateTime date, ValidationContext context) =>
+        ValidateValidDateTimeLocal(date,nameof(ValidBeginDateTime), context);
+
+    public static ValidationResult? ValidateValidEndDateTime(DateTime date, ValidationContext context) =>
+        ValidateValidDateTimeLocal(date, nameof(ValidEndDateTime), context);
+
+    static ValidationResult? ValidateValidDateTimeLocal(DateTime date, string inspectionTargetName, ValidationContext context) =>
+        date < SqlDateTime.MinValue.Value
+            ? new ValidationResult($"{inspectionTargetName}は{SqlDateTime.MinValue.Value}以上の値を設定してください。")
+            : ValidationResult.Success;
 }
