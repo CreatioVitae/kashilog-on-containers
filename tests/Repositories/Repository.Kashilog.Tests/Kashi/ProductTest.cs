5f24a7d51254c22b12008ace419a44e82d584f7d
hchiba1984 <h.chiba201103@gmail.com>
2023-12-22 11:15:12 +0900

    nailcare.

--- tests/Repositories/Repository.Kashilog.Tests/Kashi/ProductTest.cs
+++ tests/Repositories/Repository.Kashilog.Tests/Kashi/ProductTest.cs
@@ -3,6 +3,7 @@ using DomainObject.Kashilog.Kashi.CommandParameters;
 using Repository.Kashilog.Kashi;
 using Repository.Kashilog.Tests.TestContexts;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace Repository.Kashilog.Tests.Kashi;
 public class ProductTest(RepositoryKashilogTestContext testContext) : IDisposable, IClassFixture<RepositoryKashilogTestContext> {
@@ -54,6 +55,54 @@ public class ProductTest(RepositoryKashilogTestContext testContext) : IDisposabl
 
         var e =  Assert.Throws<ValidationException>(() => ObjectValidator.ThrowIfInvalid(createParam));
         Assert.Equal(ProductsConstraintValues.ProductName.LengthOutOfRangeErrorMessage, e.Message);
+    }
+
+    [Fact(DisplayName = "ProductCreateParameters検証_適用開始日最小値未満")]
+    public void ProductCreateParametersValidationTest_CaseValidBeginDateTimeIsLessThanMinValue() {
+        var createParam = new ProductCreateParameters() {
+            ValidBeginDateTime = new DateTime(1, 1, 1),
+            ValidEndDateTime = new DateTime(2021, 12, 31),
+            ProductName = "ポッキーチョコレート",
+            LargeCategory = LargeCategory.TheWestConfectionery,
+            MiddleCategory = MiddleCategory.Chocolate,
+            SmallCategory = SmallCategory.QuasiChocolate,
+            UnitPrice = 278.0000M,
+            Amount = 9.00M,
+            AmountType = AmountType.Pack,
+            Description = $"軽快な食感に焼き上げたプレッツェルにコクのあるチョコレートをコーティング。{Environment.NewLine}ポキッとした心地よい食感のポッキーは、楽しさいっぱいの弾むおいしさです。",
+            MakerCompanyId = 1,
+            PublisherCompanyId = 1
+        };
+
+        var e = Assert.Throws<ValidationException>(() => ObjectValidator.ThrowIfInvalid(createParam));
+
+        Assert.Equal($"{nameof(ProductCreateParameters.ValidBeginDateTime)}は{SqlDateTime.MinValue.Value}以上の値を設定してください。", e.Message);
+    }
+
+
+    [Fact(DisplayName = "ProductCreateParameters検証_適用終了日最小値未満")]
+    public void ProductCreateParametersValidationTest_CaseValidEndDateTimeIsLessThanMinValue() {
+        var createParam = new ProductCreateParameters() {
+            ValidBeginDateTime = new DateTime(1, 1, 1),
+            ValidEndDateTime = new DateTime(1, 1, 1),
+            ProductName = "ポッキーチョコレート",
+            LargeCategory = LargeCategory.TheWestConfectionery,
+            MiddleCategory = MiddleCategory.Chocolate,
+            SmallCategory = SmallCategory.QuasiChocolate,
+            UnitPrice = 278.0000M,
+            Amount = 9.00M,
+            AmountType = AmountType.Pack,
+            Description = $"軽快な食感に焼き上げたプレッツェルにコクのあるチョコレートをコーティング。{Environment.NewLine}ポキッとした心地よい食感のポッキーは、楽しさいっぱいの弾むおいしさです。",
+            MakerCompanyId = 1,
+            PublisherCompanyId = 1
+        };
+
+        var e = Assert.Throws<ValidationException>(() => ObjectValidator.ThrowIfInvalid(createParam));
+
+        Assert.Equal(
+            $"{nameof(ProductCreateParameters.ValidBeginDateTime)}は{SqlDateTime.MinValue.Value}以上の値を設定してください。{Environment.NewLine}{nameof(ProductCreateParameters.ValidEndDateTime)}は{SqlDateTime.MinValue.Value}以上の値を設定してください。",
+            e.Message
+        );
     }
 
     public void Dispose() =>
