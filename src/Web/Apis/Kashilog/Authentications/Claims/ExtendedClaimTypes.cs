4ab3c2da249a4064bb327e63585e579ba5a6470b
hchiba1984 <h.chiba201103@gmail.com>
2023-12-25 09:48:41 +0900

    nailcare.

--- src/Web/Apis/Kashilog/Authentications/Claims/ExtendedClaimTypes.cs
+++ src/Web/Apis/Kashilog/Authentications/Claims/ExtendedClaimTypes.cs
@@ -3,9 +3,6 @@ namespace Api.Kashilog.Authentications.Claims;
 public static class ExtendedClaimTypes {
     const string ClaimTypesKashilogNamespace = "http://schemas.kashilog.co.jp/members/ws/2016/08/web-front-identity/claims";
 
-    public const string HashedUid = ClaimTypesKashilogNamespace + "/hashed-uid";
-    public const string OzName = ClaimTypesKashilogNamespace + "/oz-name";
-    public const string UniqueName = ClaimTypesKashilogNamespace + "/unique-name";
     public const string LastName = ClaimTypesKashilogNamespace + "/last-name";
     public const string LastNameKana = ClaimTypesKashilogNamespace + "/last-name-kana";
     public const string FirstName = ClaimTypesKashilogNamespace + "/first-name";
@@ -18,13 +15,4 @@ public static class ExtendedClaimTypes {
     public const string Building = ClaimTypesKashilogNamespace + "/building";
     public const string Gender = ClaimTypesKashilogNamespace + "/gender";
     public const string Birthday = ClaimTypesKashilogNamespace + "/birthday";
-    public const string IsMarried = ClaimTypesKashilogNamespace + "/is-married";
-    public const string OccupationId = ClaimTypesKashilogNamespace + "/occupation-id";
-    public const string OccupationName = ClaimTypesKashilogNamespace + "/occupation-name";
-    public const string WebMemberId = ClaimTypesKashilogNamespace + "/web-member-id";
-    public const string WebMemberEmail = ClaimTypesKashilogNamespace + "/web-member-email";
-    public const string MobileMemberId = ClaimTypesKashilogNamespace + "/mobile-member-id";
-    public const string MobileMemberEmail = ClaimTypesKashilogNamespace + "/mobile-member-email";
-    public const string IsYumenaviMember = ClaimTypesKashilogNamespace + "/is-yumenavi-member";
-    public const string IsSaisonMember = ClaimTypesKashilogNamespace + "/is-saison-member";
 }
